cfb887f5283d11dfb5feed0e853e5cd6ec745a98
Gregory Vogel <gmv23@cornell.edu>
2020-10-23 17:04:16 -0400

    Messing around with adding "gene track" to LD and p-value figures

---
+++ PepperGWASFigures.R
@@ -0,0 +1,238 @@
+setwd("~/Documents/work/Smart_lab/P_capsici/Pepper_Interactions/paper/gwas/")
+library(grid)
+library(qqman)
+
+#######################################          Load and clean data       ######################################
+
+#Read geno as 012 file
+geno <- read.table("../geno/data/capsici_pepper_subset.012")
+geno$V1 <- NULL #Get rid of row names
+geno <- as.matrix(geno)
+geno[geno==-1] <- NA
+
+#Read sample names
+indvs <- read.table("../geno/data/capsici_pepper_subset.012.indv")
+indvs <- as.character(unlist(indvs$V1))
+rownames(geno) <- indvs
+
+#Read virulence BLUEs
+phenos <- read.csv("data/virulence_blues.csv")
+rownames(phenos) <- phenos$Isolate
+phenos$Isolate <- NULL
+
+#Read GWAS pvalues
+pvals <- read.csv("data/gwas_pvalues.csv")
+
+#Put genos and phenos in same order
+phenos <- phenos[match(rownames(geno), rownames(phenos)),]
+
+#Subset pvals into snps and pvals and rename "main" trait
+snps <- pvals[,c("CHROM","BP","MARKER")]
+pvals <- pvals[,!colnames(pvals) %in% c("CHROM","BP","MARKER")]
+colnames(pvals)[colnames(pvals) == "main"] <- "Across-pepper"
+
+#Read reference genome gene annotations and additional effector annotations
+annotations <- read.delim("../../../isolate_collection/analysis/genome/annotations.txt", stringsAsFactors = F)
+effectors <- read.table("../effectors/data/Pc_effectors_blastout.txt")
+colnames(effectors) <- c("qseqid", "sseqid", "pident", "length",
+                         "mismatch", "gapopen", "qstart", "qend",
+                         "sstart", "send", "evalue", "bitscore")
+
+###########################################        Plot manhattan and qq plots        ########################################
+
+#Function to get FDR threshold
+
+fdr_cutoff <- function(x, alpha){
+  x.sort <- sort(x)
+  x.adjust <- p.adjust(x.sort, method="fdr")
+  significant <- any(x.adjust <= alpha)
+  if(significant){
+    threshold <- x.sort[max(which(x.adjust < alpha))]
+  }else{
+    threshold <- NA
+  }
+}
+
+#Function to get plot coordinates for pane letters
+get_coords <- function(x=-0.05,y=1.3){
+  require(grid)
+  x.coord <- grconvertX(x, "npc", "user")
+  y.coord <- grconvertY(y, "npc", "user")
+  return(c(x.coord, y.coord))
+}
+
+#Function to draw stacked manhattan and qq plots
+draw_plots <- function(pvals.plot, name){
+  
+  n.plots <- ncol(pvals.plot)
+  jpeg(name, width=7, height=(1.5*n.plots), units="in",res=100)
+  
+  old.par <- par(no.readonly = T)
+  par(mar=c(6.5,4.5,1,1.5), oma=c(0,1,1.5,1))
+  
+  #Get layout
+  m <- c(rep(seq(1,((n.plots*2)-1),by=2),4),
+         seq(2,(n.plots*2),by=2))
+  m <- sort(m)
+  m <- matrix(m, nrow=n.plots, ncol=5, byrow=T)
+  layout(m)
+  
+  #Plot
+  for(i in 1:n.plots){
+    trait <- colnames(pvals.plot)[i]
+    man.df <- data.frame("BP"=snps$BP, "CHR"=snps$CHROM,"P"=pvals.plot[,i])
+    threshold <- -log10(fdr_cutoff(man.df$P,alpha=0.10))
+    if(is.na(threshold)){
+      genomewideline <- FALSE
+    }else{
+      genomewideline <- threshold
+    }
+    if(i < n.plots){
+      xlab.man <- ""
+      xlab.qq <- ""
+    }else{
+      xlab.man <- "Scaffold"
+      xlab.qq <- expression(paste("Observed -log"[10], "(",italic("p"), ")", sep=""))
+    }
+    manhattan(man.df,main=trait,suggestiveline=FALSE,
+              genomewideline=genomewideline,
+              xlab = xlab.man,
+              ylim=c(0,-log10(min(pvals.plot))*1.1))
+    par(xpd=NA)
+    text(get_coords()[1], get_coords()[2], LETTERS[i], cex=1.7)
+    par(xpd=F)
+    qq(man.df$P, main=trait, xlab=xlab.qq, ylab="")
+    mtext("Expected", side=2, line=3.5, cex=0.65)
+    mtext(expression(paste("-log"[10], "(",italic("p"),")")), side=2, line=2.25, cex=0.65)
+    
+  }
+  
+  par(old.par)
+  
+  dev.off()
+  
+}
+
+#Separate into peppers with significant hits and those without
+thresholds <- apply(pvals, 2, fdr_cutoff, alpha = 0.10)
+pvals.sig <- pvals[,!is.na(thresholds)]
+pvals.insig <- pvals[,is.na(thresholds)]
+
+#Make plots
+draw_plots(pvals.sig, "plots/GWAS_sig.jpeg")
+draw_plots(pvals.insig, "plots/GWAS_insig.jpeg")
+
+###########################################        Plot SNP effects       ########################################
+
+#Which is the significant SNP we identified
+sig_snp <- which(pvals$RedKnight == min(pvals$RedKnight))
+sig_geno <- as.factor(geno[,sig_snp])
+
+get_coords <- function(x=-0.15,y=1.3){
+  require(grid)
+  x.coord <- grconvertX(x, "npc", "user")
+  y.coord <- grconvertY(y, "npc", "user")
+  return(c(x.coord, y.coord))
+}
+
+pdf("plots/snp_effects.pdf")
+
+old.par <- par(no.readonly = T)
+par(mfrow=c(3,3))
+
+for(i in 1:9){
+  boxplot.data <- boxplot(phenos[,i] ~ sig_geno, plot=F)
+  boxplot(phenos[,i] ~ sig_geno, main=colnames(pvals)[i],
+          xlab = "Genotype",
+          ylab = "AUDPC", 
+          names = c("CC", "CA", "AA"), outline=F)
+  stripchart(phenos[,i] ~ sig_geno, vertical=T, pch=1, add=T, method="jitter", cex=1)
+  par(xpd=NA)
+  text(get_coords()[1], get_coords()[2], LETTERS[i], cex=1.5)
+  par(xpd=F)
+}
+
+par(old.par)
+dev.off()
+
+####################################    Show LD to peak SNPs in significant regions    ##################################
+
+peak_bp <- snps[sig_snp,"BP"]
+
+#Show peak +/- this distance, and for this trait
+distance <- 100000
+trait <- "RedKnight"
+
+filter <- snps$CHROM == 39 & snps$BP > (peak_bp-distance) & snps$BP <= (peak_bp+distance)
+n <- sum(filter)
+
+#Subset snps, genotypes, and pvals
+snps.sub <- snps[filter,]
+pvals.sub <- pvals[filter,trait]
+geno.sub <- geno[,filter]
+
+#get -log10 pvalues
+logp <- -log10(pvals.sub)
+
+#Get LD to peak SNP
+peak <- which(logp == max(logp))
+geno.peak <- geno.sub[,peak]
+lds <- rep(NA, n)
+for(i in 1:n){
+  lds[i] <- cor(geno.sub[,i], geno.peak, use = "complete.obs")^2
+}
+
+#rescale r2 distribution to -log10 distribution for plotting
+lds.rescale <- (max(logp)-min(logp))/(max(lds)-min(lds)) * (lds-max(lds)) + max(logp)
+
+#Now subset gene info
+anno.sub <- annotations[annotations$chrom=="PHYCAscaffold_39" & 
+                          apply(annotations[,c("start","end")],1,min) > (peak_bp-distance) &
+                          apply(annotations[,c("start","end")],1,min) < (peak_bp+distance),]
+eff.sub <- effectors[effectors$sseqid=="PHYCAscaffold_39" & 
+                       apply(effectors[,c("sstart","send")],1,min) > (peak_bp-distance) &
+                       apply(effectors[,c("sstart","send")],1,min) < (peak_bp+distance),]
+# Plot, with gene "track" on bottom
+
+pdf("plots/LD_with_SNP.pdf")
+
+m <- rbind(1,1,1,1,2)
+layout(m)
+
+old.par <- par(no.readonly=T)
+par(mar=c(5,5,3,5))
+
+#Plot 1: p-values and LD
+plot(0, type="n", 
+     xlim = range(snps.sub$BP), 
+     ylim=c(0, max(logp)), xaxt="n",
+     ylab = expression(paste("-log"[10], "(", italic("p"), ")")),
+     xlab = "Physical position (Kb) on scaffold 39")
+for(r in 1:n){
+  lines(x=rep(snps.sub$BP[r],2), y = c(0,logp[r]), col='gray')
+}
+points(snps.sub$BP, lds.rescale, pch=2)
+points(snps.sub$BP[peak], lds.rescale[peak], pch=17, cex=0.9, col='orange')
+axis(side=4, at = seq(0,max(logp), length.out=6), labels = seq(0,max(lds), length.out=6))
+axis(side=1, at = seq(100000,1000000,by=100000), labels = seq(100000,1000000,by=100000)/1000)
+mtext(expression(italic("r")^2), side=4, line=3)
+
+#Plot 2: Gene track
+plot(0, type='n', xaxt='n', yaxt='n',xlab='',main='',ylab='',bty='n',
+     xlim=range(snps.sub$BP),
+     ylim=c(0,10))
+n.genes <- nrow(anno.sub)
+for(i in 1:n.genes){
+  if(!is.na(anno.sub$signalP[i])){
+    gene.col <- "red"
+  }else{
+    gene.col <- "black"
+  }
+  track <- 5 + runif(1,min=-3,max=3)
+  lines(c(anno.sub$start[i], anno.sub$end[i]),
+        c(track,track),
+        col=gene.col,lwd=2)
+}
+
+par(old.par)
+dev.off()
--- TwoStepGWAS.R
+++ TwoStepGWAS.R
@@ -196,135 +196,12 @@ write.csv(gwas_models, "tables/gwas_models.csv", quote=F, row.names=F)
 pvals_write <- cbind(snps, pvals)
 write.csv(pvals_write, "data/gwas_pvalues.csv", quote=F, row.names=F)
 
-###########################################        Plot results        ########################################
-
-#Function to get plot coordinates for pane letters
-get_coords <- function(x=-0.05,y=1.3){
-  require(grid)
-  x.coord <- grconvertX(x, "npc", "user")
-  y.coord <- grconvertY(y, "npc", "user")
-  return(c(x.coord, y.coord))
-}
-
-#Function to draw stacked manhattan and qq plots
-draw_plots <- function(pvals.plot, name){
-  
-  require(qqman)  
-  require(grid)
-  
-  n.plots <- ncol(pvals.plot)
-  jpeg(name, width=7, height=(1.5*n.plots), units="in",res=100)
-  
-  old.par <- par(no.readonly = T)
-  par(mar=c(6.5,4.5,1,1.5), oma=c(0,1,1.5,1))
-
-  #Get layout
-  m <- c(rep(seq(1,((n.plots*2)-1),by=2),4),
-         seq(2,(n.plots*2),by=2))
-  m <- sort(m)
-  m <- matrix(m, nrow=n.plots, ncol=5, byrow=T)
-  layout(m)
-  
-  #Plot
-  for(i in 1:n.plots){
-    trait <- colnames(pvals.plot)[i]
-    man.df <- data.frame("BP"=snps$BP, "CHR"=snps$CHROM,"P"=pvals.plot[,i])
-    threshold <- -log10(fdr_cutoff(man.df$P,alpha=0.10))
-    if(is.na(threshold)){
-      genomewideline <- FALSE
-    }else{
-      genomewideline <- threshold
-    }
-    if(i < n.plots){
-      xlab.man <- ""
-      xlab.qq <- ""
-    }else{
-      xlab.man <- "Scaffold"
-      xlab.qq <- expression(paste("Observed -log"[10], "(",italic("p"), ")", sep=""))
-    }
-    manhattan(man.df,main=trait,suggestiveline=FALSE,
-              genomewideline=genomewideline,
-              xlab = xlab.man,
-              ylim=c(0,-log10(min(pvals.plot))*1.1))
-    par(xpd=NA)
-    text(get_coords()[1], get_coords()[2], LETTERS[i], cex=1.7)
-    par(xpd=F)
-    qq(man.df$P, main=trait, xlab=xlab.qq, ylab="")
-    mtext("Expected", side=2, line=3.5, cex=0.65)
-    mtext(expression(paste("-log"[10], "(",italic("p"),")")), side=2, line=2.25, cex=0.65)
-
-  }
-  
-  par(old.par)
-  
-  dev.off()
-  
-}
-
-#Move 'main' to beginning of pvals
-pvals.order <- cbind("Across-pepper" = pvals$main, pvals)
-pvals.order$main <- NULL
-
-#Separate into peppers with significant hits and those without
-thresholds <- apply(pvals.order, 2, fdr_cutoff, alpha = 0.10)
-pvals.sig <- pvals.order[,!is.na(thresholds)]
-pvals.insig <- pvals.order[,is.na(thresholds)]
-
-#Make plots
-draw_plots(pvals.sig, "plots/GWAS_sig.jpeg")
-draw_plots(pvals.insig, "plots/GWAS_insig.jpeg")
-
-###########################################        Summarize results       ########################################
+#### Get and write SNP p-values, R2s, and additive allelic effects ###
 
 #Which is the significant SNP we identified
 sig_snp <- which(pvals$RedKnight == min(pvals$RedKnight))
+sig_geno <- geno[,sig_snp]
 
-
-#This SNP is top X % in each of the traits
-sig_ps <- unlist(pvals[sig_snp,])
-percentiles <- rep(NA, length(sig_ps))
-names(percentiles) = colnames(pvals)
-for(i in 1:ncol(pvals)){
-  percentiles[i] <- sum(pvals[,i] <= sig_ps[i])/nrow(pvals)
-}
-percentiles*100
-
-#Effect plots
-sig_geno <- as.factor(geno[,sig_snp])
-colnames(pvals)[9] <- "Across Pepper"
-
-library(grid)
-get_coords <- function(x=-0.15,y=1.3){
-  require(grid)
-  x.coord <- grconvertX(x, "npc", "user")
-  y.coord <- grconvertY(y, "npc", "user")
-  return(c(x.coord, y.coord))
-}
-
-pdf("plots/snp_effects.pdf")
-
-old.par <- par(no.readonly = T)
-par(mfrow=c(3,3))
-
-for(i in 1:9){
-  boxplot.data <- boxplot(phenos[,i] ~ sig_geno, plot=F)
-  boxplot(phenos[,i] ~ sig_geno, main=colnames(pvals)[i],
-          xlab = "Genotype",
-          ylab = "AUDPC", 
-          names = c("CC", "CA", "AA"), outline=F)
-  stripchart(phenos[,i] ~ sig_geno, vertical=T, pch=1, add=T, method="jitter", cex=1)
-  par(xpd=NA)
-  text(get_coords()[1], get_coords()[2], LETTERS[i], cex=1.5)
-  par(xpd=F)
-}
-
-par(old.par)
-dev.off()
-
-sig_calls <- sig_geno[!is.na(sig_geno)]
-(sum(sig_calls==1)*2 + sum(sig_calls=2)) / (2*length(sig_calls))
-
-##Get SNP p-values, R2s, and additive allelic effects
 p.summary <- data.frame("Pepper" = colnames(pvals),
                         "P" = unlist(pvals[sig_snp,]),
                         "R2" = NA,
@@ -343,41 +220,18 @@ p.summary$Allelic_effect <- sapply(p.summary$Allelic_effect, round, digits=2)
 
 write.csv(p.summary, "tables/pval_summary.csv", quote=F, row.names = F)
 
-#########################   Show LD to peak SNPs in significant regions   ################################
+###########################################        Random calculations       ########################################
 
-#Subset to scaffold 39
-scaff39.p <- pvals$RedKnight[snps$CHROM==39]
-
-#get -log10 pvalues
-scaff39.logp <- -log10(scaff39.p)
-
-#Get LD to peak SNP
-geno.scaff39 <- geno[,snps$CHROM==39]
-peak <- which(scaff39.p == min(scaff39.p))
-geno.peak <- geno.scaff39[,peak]
-lds <- rep(NA, length(scaff39.p))
-for(i in 1:length(lds)){
-  lds[i] <- cor(geno.scaff39[,i], geno.peak, use = "complete.obs")^2
+#This SNP is top X % in each of the traits
+sig_ps <- unlist(pvals[sig_snp,])
+percentiles <- rep(NA, length(sig_ps))
+names(percentiles) = colnames(pvals)
+for(i in 1:ncol(pvals)){
+  percentiles[i] <- sum(pvals[,i] <= sig_ps[i])/nrow(pvals)
 }
+percentiles*100
 
-#rescale r2 distribution to -log10 distribution for plotting
-lds.rescale <- (max(scaff39.logp)-min(scaff39.logp))/(max(lds)-min(lds)) * (lds-max(lds)) + max(scaff39.logp)
+#MAF
+sig_calls <- sig_geno[!is.na(sig_geno)]
+(sum(sig_calls==1)*2 + sum(sig_calls=2)) / (2*length(sig_calls))
 
-pdf("plots/LD_with_SNP.pdf")
-old.par <- par(no.readonly=T)
-par(mar=c(5,5,3,5))
-plot(0, type="n", 
-     xlim = range(snps$BP[snps$CHROM==39]), 
-     ylim=c(0, max(scaff39.logp)), xaxt="n",
-     ylab = expression(paste("-log"[10], "(", italic("p"), ")")),
-     xlab = "Physical position (Kb) on scaffold 39")
-for(r in 1:length(scaff39.logp)){
-  lines(x=rep(snps$BP[snps$CHROM==39][r],2), y = c(0,scaff39.logp[r]), col='gray')
-}
-points(snps$BP[snps$CHROM==39], lds.rescale, pch=2)
-points(snps$BP[snps$CHROM==39][peak], lds.rescale[peak], pch=17, cex=0.9, col='orange')
-axis(side=4, at = seq(0,max(scaff39.logp), length.out=6), labels = seq(0,max(lds), length.out=6))
-axis(side=1, at = seq(100000,1000000,by=100000), labels = seq(100000,1000000,by=100000)/1000)
-mtext(expression(italic("r")^2), side=4, line=3)
-par(old.par)
-dev.off()
